e96f9d1d0379cd4ba0feff37b1903645b77535ca
NikolaevMikhail1881 <misha2704200418@mail.ru>
2022-09-28 14:59:45 +0300

    lab2

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -1,20 +1,73 @@
-﻿// lab2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream> 
+#include <string> 
 
-#include <iostream>
+void task_1() {
+	int num;
+	std::cout << "Enter num: ";
+	std::cin >> num;
+	std::cout << "Num: " << num << "\n";
+	std::string line;
+	for (int i = 0; i <= num; i++) {
+		line += std::to_string(i);
+		std::cout << line << "\n";
+	}
+}
+
+void task_2() {
+	int n;
+	unsigned long long Cnk = 1;
+
+	std::cout << "Введите число n: ";
+	std::cin >> n;
+	std::cout << "Ваши биноминальные коэфиценты\n";
+
+	for (int k = 1; k <= n + 1; k++) {
+		std::cout << Cnk << " ";
+		Cnk = Cnk * (n + 1 - k) / k;
+	}
+	std::cout << "\n";
+}
+
+void task_3() {
+	std::string s_num;
+	int ctn = 0;
+	int summ = 0;
+
+	std::cout << "Enter num[0]: ";
+	std::cin >> s_num;
+	while (s_num != "-") {
+		summ += std::atoi(s_num.c_str());
+		ctn++;
+
+		std::cout << "Enter num[" << ctn << "]: ";
+		std::cin >> s_num;
+	}
+
+	std::cout << double(summ) / ctn << "\n";
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "RU");
+	int menu;
+	std::cout << "Enter menu mode: ";
+	std::cin >> menu;
+
+	switch (menu) {
+	case 1:
+		task_1();
+		break;
+	case 2:
+		task_2();
+		break;
+	case 3:
+		task_3();
+		break;
+	case 4:
+		std::cout << "Exit\n";
+		break;
+	default:
+		std::cout << "Exit\n";
+		break;
+	}
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
