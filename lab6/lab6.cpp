3aa74d33fd7dfb5dcb95a827b7b43f6a8f8b2545
NikolaevMikhail1881 <misha2704200418@mail.ru>
2022-11-24 23:17:45 +0300

    like

--- lab6/lab6.cpp
+++ lab6/lab6.cpp
@@ -1,10 +1,12 @@
-﻿// lab 4.2
-#include <iostream>
+﻿#include <iostream>
+#include <fstream>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
+using std::ifstream;         
+using std::ofstream;
 
 
 void task_1(char* c, int size) {
@@ -43,8 +45,8 @@ void task_2(char* c, int size) {
 }
 
 void task_3(char* c) {
-	cout << c[0] << c[1]; 
-	cout << endl; 
+	cout << c[0] << c[1];
+	cout << endl;
 }
 
 void task_4(char* c, int size) {
@@ -61,42 +63,85 @@ void task_4(char* c, int size) {
 
 		}
 	}
-	for (int i = schz - 1 ; i >= 0; i--) {
+	for (int i = schz - 1; i >= 0; i--) {
 		cout << a[i];
 	}
 	cout << endl;
 }
 
-void task_5(char*c, int size) {
+void task_5(char* c, int size) {
 	cout << "Введите новое название файла :\n";
 	string n;
 	cin >> n;
 	char* b = new char[n.length()];
+	for (int i = 0; i < n.length(); i++) {
+		b[i] = n[i];
+	}
 	char* a = new char[size];
 	int schz = 0;
 	for (int i = size; i >= 0; i--) {
 		if (c[i] == '\\') {
 			a[schz] = c[i];
 			schz++;
+
 		}
 		else if (schz > 0) {
 			a[schz] = c[i];
 			schz++;
 		}
 	}
+	int zx = 0;
+	char* m = new char[n.length() + schz + 4];
 	for (int i = schz - 1; i >= 0; i--) {
-		cout << a[i];
+		m[zx] = a[i];
+		zx++;
+	}
+
+	zx = 0;
+	for (int i = schz; i < schz + n.length(); i++) {
+		m[i] = b[zx];
+		zx++;
+	}
+
+	for (int i = 0; i < schz + n.length(); i++) {
+		cout << m[i];
+	}
+
+	for (int i = size - 4; i < size; i++) {
+		cout << c[i];
+	}
+	cout << endl;
+}
+
+bool task_6(const string file_path_full) {
+	const string file_path_full_copy = "D:\\Application Files\\test_copy.txt";
+	ifstream fin(file_path_full);
+	ofstream fout(file_path_full_copy);
+	char buff[50];
+	if (fin.is_open() && fout.is_open()) {
+		while (!fin.eof()) {
+			fin.getline(buff, 50);
+			fout << buff << endl;
+		}
+		fin.close();
+		fout.close();
+		return true;
+	}
+	else {
+		return false;
 	}
 }
 
+
 int main() {
 	setlocale(LC_ALL, "RU");         // Подключаем функцию позволяющия выводить русскоязычные слова 
+	const string file_path_full = "D:\\Application Files\\test.txt";
 	int n;
-	bool flag = true;
+	bool flag = true, task = true;
 	string filepath;
 	cout << "Введите полный путь к файлу : ";
 	cin >> filepath;
-	char *c = new char[filepath.length()];
+	char* c = new char[filepath.length()];
 	for (int i = 0; i < filepath.length(); i++) {
 		c[i] = filepath[i];
 	}
@@ -113,7 +158,17 @@ int main() {
 		case 3: task_3(c);
 			break;
 		case 4: task_4(c, filepath.length());
+			break;
 		case 5: task_5(c, filepath.length());
+			break;
+		case 6:
+			task = task_6(file_path_full);
+			if (!task) {
+				cout << "Ошибка при открытии файла!" << endl;
+			}
+			else {
+				cout << "Файл скопирован." << endl;
+			}
 			break;
 		case 0:
 			flag = false;
--- lab7/lab7.cpp
+++ lab7/lab7.cpp
@@ -1,1 +1,85 @@
 ﻿//lab 5.1
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+class Matrix {
+
+public:
+	int Row; // строки
+	int Column; // столбцы
+	double* Array; // динамический массив
+
+public:
+	void sum(const Matrix* mat2);
+	void mult(const Matrix* mat2);
+	double trace();
+	double det();
+	void mult_by_num(double num);
+	void input();
+	void print();
+	int get_columns();
+	int get_rows();
+	double get_elem(int i, int j);
+};
+
+void Matrix::input() {
+	cout << "Введите кол - во строк : ";
+	cin >> Row;
+	cout << "Введите кол - во столбцов : ";
+	cin >> Column;
+	cout << "Заполните массив : \n"; 
+	this->Array = new double[Row * Column];
+	for (int i = 0; i < Row * Column; i++) {
+		cin >> Array[i];
+	}
+}
+
+void Matrix::print() {
+	cout << "Матрица равна :" << endl; 
+	for (int i = 0; i < Row * Column; i++ ) {
+		cout << Array[i] << " "; 
+	}
+}
+
+double Matrix::trace() {
+	double sum = 0;
+	if (Row == Column) {
+		int counter = 0;
+		for (int i = 0; i < Row * Column; i++ ) {
+			if (i == counter) {
+				sum += Array[i];
+				counter += Column + 1;
+			} 
+		}
+		return sum; 
+	}
+	else {
+		cout << "Матрица не является квадратной ! \n";
+		return sum;
+	}
+}
+
+int Matrix::get_rows() {
+	return Row; 
+}
+int Matrix::get_columns() {
+	return Column;
+}
+double Matrix::get_elem(int i, int j) {
+	return 
+}
+int main() {
+	setlocale(LC_ALL, "RU");
+	Matrix test;
+	test.input();
+	double summary = test.trace();
+	cout << "След матрицы : " << summary; 
+	return 0;
+}
+
+
