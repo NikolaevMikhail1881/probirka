2c4fa08085cf5d59b690719ec26dca2047015ab1
NikolaevMikhail1881 <misha2704200418@mail.ru>
2022-12-26 23:10:04 +0300

    likes

---
+++ lab 14/lab 14.cpp
@@ -0,0 +1,38 @@
+﻿// lab  6.4
+
+#include <iostream>
+#include <set>
+#include <string>
+
+int main()
+{
+    std::set <std::string> data_set;
+    std::set <std::string> data_rep;
+    std::string name;
+    std::cout << "Enter student name: ";
+    std::cin >> name;
+
+    // заполнение множеств
+    while (name != "-") {
+        if (data_set.find(name) != data_set.end())
+            data_rep.insert(name);
+        data_set.insert(name);
+
+        std::cout << "Enter student name: ";
+        std::cin >> name;
+    }
+
+    //отчиска основного множества
+    for (auto i : data_rep)
+        data_set.erase(i);
+
+    // вывод итогового множества
+    std::cout << "\nAnswer:\n";
+    for (auto i : data_set)
+        std::cout << "\x1b[32m[+] " << i << "\x1b[37m\n";
+
+    // вывод хитрых 
+    for (auto i : data_rep)
+        std::cout << "\x1b[31m[-] " << i << "\x1b[37m\n";
+
+}
